68f10a9c8c0a395beaa6f74990abf5ab3f0de6bf
macoto1995 <adosute07@gmail.com>
2022-06-18 13:33:22 +0900

    layout

---
+++ web/src/layouts/BlogLayout/BlogLayout.tsx
@@ -0,0 +1,27 @@
+import { Link, routes } from '@redwoodjs/router'
+type BlogLayoutProps = {
+  children?: React.ReactNode
+}
+
+const BlogLayout = ({ children }: BlogLayoutProps) => {
+  return (
+    <>
+      <header>
+        <Link to={routes.home()}>Redwood Blog</Link>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.home()}>Home</Link>
+            </li>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>{children}</main>
+    </>
+  )
+}
+
+export default BlogLayout
--- web/src/pages/AboutPage/AboutPage.tsx
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -1,27 +1,13 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const AboutPage = () => {
   return (
     <>
       <MetaTags title="About" description="About page" />
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <p>
-          This site was created to demonstrate my mastery of Redwood: Look on my
-          works, ye mighty, and despair!
-        </p>
-      </main>
-      <Link to={routes.home()}>Return Home</Link>
+      <p>
+        This site was created to demonstrate my mastery of Redwood: Look on my
+        works, ye mighty, and despair!
+      </p>
     </>
   )
 }
