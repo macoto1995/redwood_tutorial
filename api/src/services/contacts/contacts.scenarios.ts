f3e5bb8d5b50329ae3d4cfaee47d8e346a39ae9b
macoto1995 <adosute07@gmail.com>
2022-06-19 16:41:10 +0900

    made tests pass

--- api/src/services/contacts/contacts.scenarios.ts
+++ api/src/services/contacts/contacts.scenarios.ts
@@ -2,8 +2,12 @@ import type { Prisma } from '@prisma/client'
 
 export const standard = defineScenario<Prisma.ContactCreateArgs>({
   contact: {
-    one: { data: { name: 'String', email: 'String', message: 'String' } },
-    two: { data: { name: 'String', email: 'String', message: 'String' } },
+    one: {
+      data: { name: 'String', email: 'test1@example.com', message: 'String' },
+    },
+    two: {
+      data: { name: 'String', email: 'test2@example.com', message: 'String' },
+    },
   },
 })
 
--- web/src/components/Article/Article.test.tsx
+++ web/src/components/Article/Article.test.tsx
@@ -8,7 +8,16 @@ import Article from './Article'
 describe('Article', () => {
   it('renders successfully', () => {
     expect(() => {
-      render(<Article />)
+      render(
+        <Article
+          article={{
+            id: 42,
+            title: 'First Post',
+            body: `Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Street art next level umami squid. Hammock hexagon glossier 8-bit banjo. Neutra la croix mixtape echo park four loko semiotics kitsch forage chambray. Semiotics salvia selfies jianbing hella shaman. Letterpress helvetica vaporware cronut, shaman butcher YOLO poke fixie hoodie gentrify woke heirloom.`,
+            createdAt: '2020-01-01T12:34:45Z',
+          }}
+        />
+      )
     }).not.toThrow()
   })
 })
