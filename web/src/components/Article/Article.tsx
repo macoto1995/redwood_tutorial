f14ccf3c6b8a096107b9d664bc1f085742c80c01
macoto1995 <adosute07@gmail.com>
2022-06-19 16:33:42 +0900

    stories

--- web/src/components/Article/Article.tsx
+++ web/src/components/Article/Article.tsx
@@ -2,16 +2,20 @@ import type { Post } from 'types/graphql'
 
 import { Link, routes } from '@redwoodjs/router'
 
+const truncate = (text: string, length: number) => {
+  return text.substring(0, length) + '...'
+}
 interface Props {
   article: Post
+  summary?: boolean
 }
-const Article = ({ article }: Props) => {
+const Article = ({ article, summary = false }: Props) => {
   return (
     <article key={article.id}>
       <header>
         <Link to={routes.article({ id: article.id })}>{article.title}</Link>
       </header>
-      <p>{article.body}</p>
+      <p>{summary ? truncate(article.body, 100) : article.body}</p>
       <div>Posted at: {article.createdAt}</div>
     </article>
   )
--- web/src/components/ArticlesCell/ArticlesCell.mock.ts
+++ web/src/components/ArticlesCell/ArticlesCell.mock.ts
@@ -1,4 +1,17 @@
 // Define your own mock data here:
 export const standard = (/* vars, { ctx, req } */) => ({
-  articles: [{ id: 42 }, { id: 43 }, { id: 44 }],
+  articles: [
+    {
+      id: 42,
+      title: 'First Post',
+      body: `Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Street art next level umami squid. Hammock hexagon glossier 8-bit banjo. Neutra la croix mixtape echo park four loko semiotics kitsch forage chambray. Semiotics salvia selfies jianbing hella shaman. Letterpress helvetica vaporware cronut, shaman butcher YOLO poke fixie hoodie gentrify woke heirloom.`,
+      createdAt: '2020-01-01T12:34:45Z',
+    },
+    {
+      id: 43,
+      title: 'Second Post',
+      body: `At your nearby Taco Bell restaurant you can find all of your favorite Mexican inspired tacos, from classic crunchy tacos and steak tacos to your new favorites like the Chalupa Supreme, and Nacho Cheese Doritos® Locos Tacos. Customize any of our tacos with a variety of sauces and add-ons to create the ultimate taco for you and your friends. With breakfast options at select locations to late night, Taco Bell serves made-to-order and customizable tacos so you can satisfy your taco craving throughout the day.`,
+      createdAt: '2020-01-01T12:34:45Z',
+    },
+  ],
 })
